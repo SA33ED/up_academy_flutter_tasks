d437227edd3410a1359b1363bc0672b72d17f532
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-09-06 16:40:28 +0300

    commit

--- chat_app/lib/features/on_boarding/presentation/widgets/get_on_boarding_btns.dart
+++ chat_app/lib/features/on_boarding/presentation/widgets/get_on_boarding_btns.dart
@@ -5,7 +5,61 @@ import 'package:chat_app/features/on_boarding/data/models/on_boarding_model.dart
 import 'package:flutter/material.dart';
 
 getOnBoardingBtns(context, currentIndex) {
-  if (currentIndex != onBoardingList.length - 1) {
+  if (currentIndex == onBoardingList.length - 1) {
+    return GestureDetector(
+      onTap: () async {
+        await getIt<AppCache>()
+            .setData(key: "sharedPreOnBoarding", value: true)
+            .then(
+              (value) => {
+                Navigator.pushReplacementNamed(context, "SignIn"),
+              },
+            );
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: MediaQuery.of(context).size.width,
+        height: 60,
+        decoration: ShapeDecoration(
+          color: const Color(0xFF6D67E4),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: Text(
+          'Start',
+          textAlign: TextAlign.center,
+          style: CustomTextStyle.poppinsStyle16.copyWith(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  } else if (currentIndex == 0) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          width: 125,
+          height: 60,
+          decoration: ShapeDecoration(
+            color: const Color(0xFF6D67E4),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: Text(
+            'Next',
+            textAlign: TextAlign.center,
+            style: CustomTextStyle.poppinsStyle16.copyWith(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  } else {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -42,36 +96,6 @@ getOnBoardingBtns(context, currentIndex) {
           ),
         ),
       ],
-    );
-  } else {
-    return GestureDetector(
-      onTap: () async {
-        await getIt<AppCache>()
-            .setData(key: "sharedPreOnBoarding", value: true)
-            .then(
-              (value) => {
-                Navigator.pushReplacementNamed(context, "SignIn"),
-              },
-            );
-      },
-      child: Container(
-        alignment: Alignment.center,
-        width: MediaQuery.of(context).size.width,
-        height: 60,
-        decoration: ShapeDecoration(
-          color: const Color(0xFF6D67E4),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        child: Text(
-          'Start',
-          textAlign: TextAlign.center,
-          style: CustomTextStyle.poppinsStyle16.copyWith(
-            color: Colors.white,
-          ),
-        ),
-      ),
     );
   }
 }
